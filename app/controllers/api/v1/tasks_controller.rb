36a1bc334e75e20e0966fe36d2c6459ddf8c00fb
scubaguba345 <brandon.gubitosa@gmail.com>
2020-09-03 16:32:06 -0400

    brandon

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::TasksController < ApplicationController
+
+    def index
+        tasks = Task.all
+
+        render json: tasks
+    end
+
+    def show
+        task = Task.find(params[:id])
+    end
+
+    def create
+        task = Task.create!(task_params)
+
+        render json: task
+    end
+
+    def destroy
+        task = Task.find(params[:id])
+
+        monster.destroy!
+
+        render json: {}
+    end
+
+    private
+
+    def monster_params
+        params.require(:task).permit(:text, :category)
+    end
+
+
+end
