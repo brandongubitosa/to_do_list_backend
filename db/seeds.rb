7c475f120b04bbd4accfbb7d2d38c17dfd2cc05c
scubaguba345 <brandon.gubitosa@gmail.com>
2020-09-03 17:38:42 -0400

    changed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Task.destroy_all
+
+myUser = User.create(username: "brandon", password:"1234")
+
+
+# tasks: [
+#     {
+#       id: 1,  
+#       text: 'Buy rice',
+#       category: 'Food'
+#     },
+#     {
+#       id: 2,
+#       text: 'Save a tenner',
+#       category: 'Money'
+#     },
+#     {
+#       id: 3,
+#       text: 'Build a todo app',
+#       category: 'Code'
+#     },
+#     {
+#       id:4,
+#       text: 'Build todo API',
+#       category: 'Code'
+#     },
+#     {
+#       id: 5,
+#       text: 'Get an ISA',
+#       category: 'Money'
+#     },
+#     {
+#       id: 6,
+#       text: 'Cook rice',
+#       category: 'Food'
+#     },
+#     { 
+#       id: 7,
+#       text: 'Tidy house',
+#       category: 'Misc'
+#     }
